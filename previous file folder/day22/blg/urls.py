db262f5626fa83c92d4a2a3d455dac194ae24757
nowshedahmed <nowshedahmed59@gmail.com>
2024-01-08 19:12:08 +0600

    new update

---
+++ previous file folder/day22/blg/urls.py
@@ -0,0 +1,27 @@
+from django.contrib import admin
+from django.urls import path
+from . import views
+
+
+
+#>>>>>  this is "Shop" Urls
+
+
+urlpatterns = [
+    path('',views.indexblg,name="blghome")
+   
+]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
